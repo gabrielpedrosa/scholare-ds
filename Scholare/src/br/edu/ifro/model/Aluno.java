42fb96830a162204a8f6e89beb6d77e5b292e1a6
gabriel.pedrosa <gabriel.pedrosaf@gmail.com>
2018-10-23 04:38:56 -0400

    Reiniciando Projeto de DS

--- Scholare/src/br/edu/ifro/model/Aluno.java
+++ Scholare/src/br/edu/ifro/model/Aluno.java
@@ -2,5 +2,149 @@ package br.edu.ifro.model;
 
 //@author Gabriel
 public class Aluno {
-    
+    private int alu_id;
+    private String alu_nome;
+    private String alu_cpf;
+    private String alu_rg;
+    private String alu_nascimento;
+    private String alu_sexo;
+    private String alu_cidade_origem;
+    private String alu_estado_origem;
+    private String alu_filiacao1;
+    private String alu_filiacao2;
+    private String alu_cidade_atual;
+    private String alu_estado_atual;
+    private String alu_logradouro;
+    private String alu_bairro;
+    private String alu_numero;
+    private String alu_complemento;
+
+    public int getAlu_id() {
+        return alu_id;
+    }
+
+    public void setAlu_id(int alu_id) {
+        this.alu_id = alu_id;
+    }
+
+    public String getAlu_nome() {
+        return alu_nome;
+    }
+
+    public void setAlu_nome(String alu_nome) {
+        this.alu_nome = alu_nome;
+    }
+
+    public String getAlu_cpf() {
+        return alu_cpf;
+    }
+
+    public void setAlu_cpf(String alu_cpf) {
+        this.alu_cpf = alu_cpf;
+    }
+
+    public String getAlu_rg() {
+        return alu_rg;
+    }
+
+    public void setAlu_rg(String alu_rg) {
+        this.alu_rg = alu_rg;
+    }
+
+    public String getAlu_nascimento() {
+        return alu_nascimento;
+    }
+
+    public void setAlu_nascimento(String alu_nascimento) {
+        this.alu_nascimento = alu_nascimento;
+    }
+
+    public String getAlu_sexo() {
+        return alu_sexo;
+    }
+
+    public void setAlu_sexo(String alu_sexo) {
+        this.alu_sexo = alu_sexo;
+    }
+
+    public String getAlu_cidade_origem() {
+        return alu_cidade_origem;
+    }
+
+    public void setAlu_cidade_origem(String alu_cidade_origem) {
+        this.alu_cidade_origem = alu_cidade_origem;
+    }
+
+    public String getAlu_estado_origem() {
+        return alu_estado_origem;
+    }
+
+    public void setAlu_estado_origem(String alu_estado_origem) {
+        this.alu_estado_origem = alu_estado_origem;
+    }
+
+    public String getAlu_filiacao1() {
+        return alu_filiacao1;
+    }
+
+    public void setAlu_filiacao1(String alu_filiacao1) {
+        this.alu_filiacao1 = alu_filiacao1;
+    }
+
+    public String getAlu_filiacao2() {
+        return alu_filiacao2;
+    }
+
+    public void setAlu_filiacao2(String alu_filiacao2) {
+        this.alu_filiacao2 = alu_filiacao2;
+    }
+
+    public String getAlu_cidade_atual() {
+        return alu_cidade_atual;
+    }
+
+    public void setAlu_cidade_atual(String alu_cidade_atual) {
+        this.alu_cidade_atual = alu_cidade_atual;
+    }
+
+    public String getAlu_estado_atual() {
+        return alu_estado_atual;
+    }
+
+    public void setAlu_estado_atual(String alu_estado_atual) {
+        this.alu_estado_atual = alu_estado_atual;
+    }
+
+    public String getAlu_logradouro() {
+        return alu_logradouro;
+    }
+
+    public void setAlu_logradouro(String alu_logradouro) {
+        this.alu_logradouro = alu_logradouro;
+    }
+
+    public String getAlu_bairro() {
+        return alu_bairro;
+    }
+
+    public void setAlu_bairro(String alu_bairro) {
+        this.alu_bairro = alu_bairro;
+    }
+
+    public String getAlu_numero() {
+        return alu_numero;
+    }
+
+    public void setAlu_numero(String alu_numero) {
+        this.alu_numero = alu_numero;
+    }
+
+    public String getAlu_complemento() {
+        return alu_complemento;
+    }
+
+    public void setAlu_complemento(String alu_complemento) {
+        this.alu_complemento = alu_complemento;
+    }
+        
 }
