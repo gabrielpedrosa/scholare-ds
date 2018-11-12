8e7ce255b51ac9d87cabb930437f5f6bb9811867
80923755268 <gabriel.pedrosaf@gmail.com>
2018-11-12 15:16:26 -0400

    Editar Funcionario (control)

--- Scholare/src/Banco_de_Dados/scholare.sql
+++ Scholare/src/Banco_de_Dados/scholare.sql
@@ -2,5 +2,9 @@ drop database if exists scholare;
 create database if not exists scholare;
 use scholare;
 
-select * from Professor;
-select * from Turma;
+select * from Funcionario;
+select * from Login;
+select * from matricula;
+select * from disciplina;
+
+insert into disciplina (null, '1000','nome');
